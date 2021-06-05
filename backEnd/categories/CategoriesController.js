00060058631425db012d8bec2a6b59d9438cc712
Roberto <mitras25jpn@hotmail.com>
2021-06-05 15:57:00 -0300

    Editar Categoria

--- backEnd/categories/CategoriesController.js
+++ backEnd/categories/CategoriesController.js
@@ -4,7 +4,7 @@ const Category = require('./Category')
 const slugify = require('slugify')
 
 
-router.get('/categories', async (req, res, next) => {
+router.get('/admin/categories', async (req, res, next) => {
   try {
     const existe = await Category.findAll();
     res.status(200);
@@ -17,36 +17,71 @@ router.get('/categories', async (req, res, next) => {
 
 router.post('/admin/categories/save', (req, res) => {
   const title = req.body.title
-  if(title != undefined){
+  if (title != undefined) {
     Category.create({
       title: title,
       //versão do titulo pra url
       slug: slugify(title)
-    }).then(()=>{
+    }).then(() => {
       res.redirect('/')
     })
-  }else{
+  } else {
     res.redirect('/categories')
   }
 })
 
-router.delete('/admin/categories/delete', (req, res)=>{
+router.delete('/admin/categories/delete', (req, res) => {
   const id = req.body.id
-  if(id != undefined){
-    if(!isNaN(id)){
+  if (id != undefined) {
+    if (!isNaN(id)) {
       Category.destroy({
-        where: {id: id}
-      }).then(()=>{
+        where: { id: id }
+      }).then(() => {
         res.send('Deletado com sucesso')
         res.redirect('/admin/categories')
       })
-    }else{ //se não for número
+    } else { //se não for número
       res.redirect('/admin/categories')
     }
-  }else{// se for nulo ou indefinido
+  } else {// se for nulo ou indefinido
     res.redirect('/admin/categories')
   }
 })
+
+router.get('/admin/categories/edit/:id', (req, res)=>{
+  const id = req.params.id
+  if(isNaN(id)){
+    res.redirect('/admin/categories')
+  }
+
+  Category.findByPk(id).then(category =>{
+    if(category != undefined){
+      res.send("achou")
+    }else{
+      res.redirect('/admin/categories')
+    }
+  }).catch(erro=>{
+    res.redirect('/admin/categories')
+  })
+})
+
+
+router.put('/admin/categories/update', (req, res) => {
+  const id = req.body.id
+  const title = req.body.title
+  console.log(req.body)
+
+  Category.update({ title: title, slug: slugify(title) }, {
+    where: {
+      id: id
+    }
+  }).then(() => {
+  res.send('Atualizado com sucesso')
+}).catch(erro => {
+  res.send(erro)
+})
+})
+
 
 
 module.exports = router
