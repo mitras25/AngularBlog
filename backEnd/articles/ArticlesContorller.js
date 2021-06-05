7031a1e381f3aacef959c9e58f77207b278d352e
Roberto <mitras25jpn@hotmail.com>
2021-06-05 17:20:55 -0300

    Artigos delete

--- backEnd/articles/ArticlesContorller.js
+++ backEnd/articles/ArticlesContorller.js
@@ -5,11 +5,14 @@ const router = express.Router()
 const Category = require('../categories/Category')
 
 // listando todos os artigos
-router.get('/admin/articles', (req, res) => {
-  Article.findAll()
-  .then(articles =>{
-    res.send('lista de artigos')
-  })
+router.get('/admin/articles', async (req, res, next) => {
+  try {
+    const existe = await Article.findAll();
+    res.status(200);
+    res.json(existe);
+  } catch (erro) {
+    next(erro);
+  }
 })
 
 //buscando as Categorias para colocar no select
@@ -41,6 +44,24 @@ router.post("/admin/articles/save", (req, res)=>{
   }).catch(erro=>{
     res.send(erro)
   })
+})
+
+router.delete('/admin/articles/delete', (req, res) => {
+  const id = req.body.id
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Article.destroy({
+        where: { id: id }
+      }).then(() => {
+        res.send('Deletado com sucesso')
+        res.redirect('/admin/articles')
+      })
+    } else { //se não for número
+      res.redirect('/admin/articles')
+    }
+  } else {// se for nulo ou indefinido
+    res.redirect('/admin/articles')
+  }
 })
 
 
