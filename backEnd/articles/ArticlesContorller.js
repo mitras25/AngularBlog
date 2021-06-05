547f90b6342779c64810de6e258005c1b96dbbfc
Roberto <mitras25jpn@hotmail.com>
2021-06-05 17:14:21 -0300

    Cadastrando Artigos

--- backEnd/articles/ArticlesContorller.js
+++ backEnd/articles/ArticlesContorller.js
@@ -1,13 +1,46 @@
 const express = require('express')
+const { default: slugify } = require('slugify')
+const Article = require('./Article')
 const router = express.Router()
+const Category = require('../categories/Category')
 
-
-router.get('/articles', (req, res) => {
-  res.send('rotas de artigos')
+// listando todos os artigos
+router.get('/admin/articles', (req, res) => {
+  Article.findAll()
+  .then(articles =>{
+    res.send('lista de artigos')
+  })
 })
 
+//buscando as Categorias para colocar no select
 router.get('/admin/articles/new', (req, res) => {
-  res.send('criou novo artigo')
+  Category.findAll({
+    //incluindo as categorias na lista
+    include: [{model: Category}]
+  })
+    .then(categories => {
+      res.send('criou novo artigo')
+    }).catch(erro => {
+      send(erro)
+    })
+})
+
+router.post("/admin/articles/save", (req, res)=>{
+  const title = req.body.title
+  const body = req.body.body
+  const category = req.body.category
+
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    categoryId: category
+  }).then(()=>{
+    res.send('Cadastrado')
+  }).catch(erro=>{
+    res.send(erro)
+  })
 })
 
 
--- backEnd/database/database.js
+++ backEnd/database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize('blog', 'root', 'meusql2021', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: "-03:00"
 })
 
 module.exports = connection
