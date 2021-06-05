4e9eb6d25ae64b63c3c200ced3f852c7290bdfa2
Roberto <mitras25jpn@hotmail.com>
2021-06-05 18:53:05 -0300

    Cadastro de usuario

--- backEnd/users/UsersController.js
+++ backEnd/users/UsersController.js
@@ -1,18 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const User = require('./User')
+const bcrypt = require('bcrypt')
 
-router.get('/admin/users', (req, res)=>{
+router.get('/admin/users', (req, res) => {
   //const usuarios = user.findAll()
   res.send('usuarios')
 })
 
-router.post('/users/create', (req,res)=>{
+router.post('/users/create', (req, res) => {
   const email = req.body.email
   const userName = req.body.userName
-  const password = req. body.passaword
+  const password = req.body.password
 
-  res.send('Cadastrado')
+
+  //criptografando senha
+  let salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(password, salt)
+
+  User.create({
+    userName: userName,
+    email: email,
+    password: hash
+  }).then(()=>{
+    res.send('Cadastrado')
+  }).catch((erro)=>{
+    res.send(erro)
+  }) 
 
 })
 
