3a1cd9acbd58effbe3c597838e4a508831465905
Roberto <mitras25jpn@hotmail.com>
2021-06-06 11:44:05 -0300

    rotas usuario

--- frontEnd/src/app/components/usuarios/usuario-create/usuario-create.component.ts
+++ frontEnd/src/app/components/usuarios/usuario-create/usuario-create.component.ts
@@ -1,15 +1,32 @@
+import { Router } from '@angular/router';
+import { UsuarioService } from './../usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../usuario.model';
 
 @Component({
   selector: 'app-usuario-create',
   templateUrl: './usuario-create.component.html',
-  styleUrls: ['./usuario-create.component.css']
+  styleUrls: ['./usuario-create.component.css'],
 })
 export class UsuarioCreateComponent implements OnInit {
+  usuario: User = {
+    userName: '',
+    email: '',
+    password: '',
+  };
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  createUser(): void {
+    this.usuarioService.create(this.usuario).subscribe(() => {
+      this.usuarioService.showMessage('Cadastrado com sucesso');
+      this.router.navigate(['/']);
+    });
   }
 
+  cancel(): void {
+    this.router.navigate(['/']);
+  }
 }
---
+++ frontEnd/src/app/components/usuarios/usuario.service.ts
@@ -0,0 +1,26 @@
+import { User } from './usuario.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+
+  baseUrl = "http://localhost:8080/users"
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
+
+  showMessage(msg: string): void {
+    this.snackBar.open(msg, 'X', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+  }
+
+  create(usuario: User): Observable<User>{
+    return this.http.post<User>(this.baseUrl, usuario)
+  }
+}
